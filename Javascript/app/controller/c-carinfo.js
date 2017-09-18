f452a08a3bd56d76c0457bd00bb621db7ccac2dd
mayoi <1299824519@qq.com>
2017-09-18 15:03:03 +0800

    modified carinfo page

--- Javascript/app/controller/c-carinfo.js
+++ Javascript/app/controller/c-carinfo.js
@@ -8,10 +8,15 @@ define(['base', 'wx'], function (BaseClass,wx) {
     function CarInfo($wrapper) {
         BaseClass.call(this, $wrapper);
 
+        var that=this;
+        //禁止页面滚动
+        that.scrollControl(false);
 
+        //显示优惠券提示
+        $(document).on(this.eventName,'.icon-help',$.proxy(this,'showTipBox'));
 
         /*点击灰色部分关闭*/
-        $(document).on(this.eventName,'.container',function(e){
+        $(document).on(this.eventName,'.help-box',function(e){
             that.toHideModuleByClickOutside(e,$.proxy(that,'hideTipBox'));
         });
 
@@ -19,6 +24,12 @@ define(['base', 'wx'], function (BaseClass,wx) {
         /*关闭*/
         $(document).on(this.eventName,'.confirm',$.proxy(that,'hideTipBox'));
 
+        //点击加载更多
+        $(document).on(this.eventName,'.load-more',function() {
+            //页面恢复滚动
+            that.scrollControl(true);
+            pt.showOrgInfo();
+        });
     };
 
 
@@ -27,13 +38,23 @@ define(['base', 'wx'], function (BaseClass,wx) {
 
     var pt = CarInfo.prototype;
 
-
+    pt.showTipBox=function(){
+        $('.help-box').addClass('show');
+    };
 
 
     pt.hideTipBox=function(){
-        $('#help-box').removeClass('show');
+        $('.help-box').removeClass('show');
     };
 
+    pt.showOrgInfo=function() {
+        var box=$('.info-item'),
+            title=$('.title'),
+            btn=$('.load-box');
+        box.removeClass('none');
+        title.removeClass('none');
+        btn.addClass('none');
+    };
 
 
     return CarInfo;
