e2da6e535d79153ed7de9b35ec73875a513a5a8b
xiaoye <kyoya-len@qq.com>
2017-09-20 16:21:05 +0800

    Update config.php

--- Conf/config.php
+++ Conf/config.php
@@ -2,16 +2,7 @@
 return array(
     //'配置项'=>'配置值'
     'TMPL_PARSE_STRING' => array(
-        '__IMG__'            => __ROOT__ . '/Application/Src/Content/images',
-        '__STYLE__'          => __ROOT__ . '/Application/Src/Content/style',
-        '__JS__'             => __ROOT__ . '/Application/Src/Javascript',
-        '__JSON__'           => __ROOT__ . '/Application/data/',
-        '__CTRL__'           => __ROOT__ . '/index.php/Src/index',
-        '__URL__'            => __ROOT__ . '/index.php',
-        '__API__'            => 'http://test.api.jiayouzan.com',
-        '__WX_AppID__'       => 'wx0cabc39032c68b6c',
-        '__Host__'           => 'http://test.web2.jiayouzan.com/',
-        '__WX_RedirectUrl__' => 'http://test.web2.jiayouzan.com/index.php/src/index/home'
+     
     ),
     'LAYOUT_ON'         => true,
     'LAYOUT_NAME'       => 'layout'
