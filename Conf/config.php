5873f9c578d6cf7ce3822683b0d22e7c5f0a84f1
mayoi <1299824519@qq.com>
2017-09-20 16:23:44 +0800

    fix

--- Conf/config.php
+++ Conf/config.php
@@ -8,10 +8,6 @@ return array(
         '__JSON__'           => __ROOT__ . '/Application/data/',
         '__CTRL__'           => __ROOT__ . '/index.php/Src/index',
         '__URL__'            => __ROOT__ . '/index.php',
-        '__API__'            => 'http://test.api.jiayouzan.com',
-        '__WX_AppID__'       => 'wx0cabc39032c68b6c',
-        '__Host__'           => 'http://test.web2.jiayouzan.com/',
-        '__WX_RedirectUrl__' => 'http://test.web2.jiayouzan.com/index.php/src/index/home'
     ),
     'LAYOUT_ON'         => true,
     'LAYOUT_NAME'       => 'layout'
