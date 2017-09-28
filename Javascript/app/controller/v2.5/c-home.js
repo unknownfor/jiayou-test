c8239fede71c6d71d6ea568d3bb74db7c43b151c
mayoi <1299824519@qq.com>
2017-09-28 10:57:00 +0800

    fix home page style

--- Javascript/app/controller/v2.5/c-home.js
+++ Javascript/app/controller/v2.5/c-home.js
@@ -4,7 +4,7 @@
  */
 
 
-define(['base','wx','wxconfig'],function (BaseClass,wx,MySlider,WxConfig,CloseBox) {
+define(['base','wx','wxconfig'],function (BaseClass,wx,WxConfig) {
     function Home($wrapper) {
 
         BaseClass.call(this,$wrapper);
@@ -18,17 +18,9 @@ define(['base','wx','wxconfig'],function (BaseClass,wx,MySlider,WxConfig,CloseBo
 
     var pt=Home.prototype;
 
-    //http://xx.com/v2/car_series/{id}/motorcycle_type
     /*获取信息*/
     pt.getHomeInfo=function(){
         this.ctrlLoadingIcon();
-        // var param={},
-        //     url='v2/car_series/{id}/motorcycle_type',
-        //     options={
-        //         errorCallback:$.proxy(this,'getInfoError')
-        //     };
-        // this.getDataAsync(url,param,$.proxy(this,'getInfoSuccess'),options);
-
         var url='../../data/home.json';
         $.getJSON(url,null,$.proxy(this,'getInfoSuccess'));
     };
@@ -37,7 +29,7 @@ define(['base','wx','wxconfig'],function (BaseClass,wx,MySlider,WxConfig,CloseBo
     /*获取成功*/
     pt.getInfoSuccess=function(data){
         // this.ctrlLoadingIcon(false);
-        //隐藏等待
+        // 隐藏等待
         // $('.homev2_5-wrapper').removeClass('hide');
         // this.hideCoverBox();
         // 隐藏菜单蒙板，
@@ -48,17 +40,19 @@ define(['base','wx','wxconfig'],function (BaseClass,wx,MySlider,WxConfig,CloseBo
     pt.showCarInfo =function (data) {
         var str = '',
             url,
+            len,
             item;
         url = window.urlObject.ctrl;
-        if(data.member_car_list.length ==0) {
-            str = '    <a class="car-item margin" href="'+ url +'/addcar'+'">' +
-                '    <div class="info-box">' +
-                '        <span class="add-car">添加我的爱车</span>' +
-                '        </div>' +
-                '    <span class="iconfont icon-right"></span>' +
-                '    </a>';
+        len=data.member_car_list.length;
+        if(data.member_car_list.length == 0) {
+            str = '<a class="car-item margin" href="'+ url +'/addcar'+'">' +
+                '<div class="info-box">' +
+                '<span class="add-car">添加我的爱车</span>' +
+                '</div>' +
+                '<span class="iconfont icon-right"></span>' +
+                '</a>';
         }else {
-            for (var i=0;i++;i<data.member_car_list.length) {
+            for (var i=0;i<len;i++) {
                 item = data.member_car_list[0];
                 str = '<a class="car-item margin" href="'+ url +'/carinfo'+'">' +
                     '<div class="icon-car">' +
@@ -74,6 +68,8 @@ define(['base','wx','wxconfig'],function (BaseClass,wx,MySlider,WxConfig,CloseBo
         }
         $('.item-box').before(str);
     };
+
+
 
 
 
