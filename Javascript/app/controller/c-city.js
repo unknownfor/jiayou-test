3c27c14890791b17a4b65316bf8cd743816fcac5
mayoi <1299824519@qq.com>
2017-11-17 09:53:20 +0800

    fix

--- Javascript/app/controller/c-city.js
+++ Javascript/app/controller/c-city.js
@@ -5,21 +5,9 @@ define(['base'],function (BaseClass) {
 
             //加载城市事件
             $('body').on('click', '#zone_ids,#gr_zone_ids', function () {
-                var zid = $(this).attr('id');
-                $('.container').show();
+                this.xx();
+            });
 
-            });
-            //选择城市 start
-            $('body').on('click', '.city-list p', function () {
-                var type = $('.container').data('type');
-                $('#zone_ids').html($(this).html()).attr('data-id', $(this).attr('data-id'));
-                $('#gr_zone_ids').html($(this).html()).attr('data-id', $(this).attr('data-id'));
-                $('.container').hide();
-            });
-            $('body').on('click', '.letter a', function () {
-                var s = $(this).html();
-                $(window).scrollTop($('#' + s + '1').offset().top);
-            });
 
         };
 
@@ -29,7 +17,9 @@ define(['base'],function (BaseClass) {
         var pt=city.prototype;
 
 
+        pt.xx = function () {
 
+        };
 
 
         return city;
